18f2656145593bff26b53e879d9cd71c7557f0e0
zhang jun <1580043700@qq.com>
2020-05-14 14:46:32 +0800

    入参限制

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,11 +1,27 @@
-import { Get, Post } from "../util/request";
+import {Get, Post} from "../util/request";
 
 /**
  * 获取城市列表
  */
-export const cityListApi = () => Get({ url: "city/list" });
+export const cityListApi = () => Get({url: "city/list"});
+
+
+interface filterHouseQuery {
+    cityId: number;
+    brandId?: number;
+    pageNum?: number;
+    teamId?: number;
+    location?: string;
+    commuter?: string;
+    fame?: string | number;
+    community?: number;
+    pageSize?: number;
+
+    [names: string]: any;
+}
+
 /**
  * 筛选房源数据
  */
-export const filterHouseApi = (params:any) =>
-  Get({ url: `house/filtrate/${params.cityId}`, params });
+export const filterHouseApi = (params: filterHouseQuery) =>
+    Get({url: `house/filtrate/${params.cityId}`, params});
--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,91 +1,92 @@
-import { Button, Table } from "antd";
+import {Button, Table} from "antd";
 import Link from "next/link";
 import classnames from "classnames";
 import Meta from "../../components/Meta";
 import Header from "../../components/Header";
 import HouseMainList from "../../components/HouseMainList";
-import { cityListApi, filterHouseApi } from "../../api/api";
+import {cityListApi, filterHouseApi} from "../../api/api";
 import styles from "./index.less";
-import { Component } from "react";
+import {Component} from "react";
 
 export default class Index extends Component<any> {
-  meta = {
-    title: "乐猪租房-成都租房",
-    meta: [
-      {
-        keywords:
-          "成都租房,成都合租,成都整租,成都民宿,成都公寓,成都短租,成都租房,租房成都,成都租房网,成都租房免中介费,成都租房网",
-      },
-      {
-        description:
-          "成都租房,成都合租,成都整租,成都民宿,成都公寓,成都短租,成都租房,租房成都,成都租房网,成都租房免中介费,成都租房网",
-      },
-      {
-        name: "viewport",
-        content: "initial-scale=1.2, width=device-width",
-      },
-    ],
-  };
-
-  static async getInitialProps({ req }) {
-    let cityList = await cityListApi();
-    let houseList = await filterHouseApi({ cityId: 1 });
-    return {
-      cityList,
-      houseList,
+    meta = {
+        title: "乐猪租房-成都租房",
+        meta: [
+            {
+                keywords:
+                    "成都租房,成都合租,成都整租,成都民宿,成都公寓,成都短租,成都租房,租房成都,成都租房网,成都租房免中介费,成都租房网",
+            },
+            {
+                description:
+                    "成都租房,成都合租,成都整租,成都民宿,成都公寓,成都短租,成都租房,租房成都,成都租房网,成都租房免中介费,成都租房网",
+            },
+            {
+                name: "viewport",
+                content: "initial-scale=1.2, width=device-width",
+            },
+        ],
     };
-  }
-  render() {
-    let { cityList, houseList } = this.props;
-    let { list } = houseList;
-    let dataSource = cityList.map((v, index) => ({
-      key: index,
-      title: v.name,
-      domain: v.domain,
-      code: v.code,
-    }));
 
-    const columns = [
-      {
-        key: "title",
-        title: "Title",
-        dataIndex: "title",
-      },
-      {
-        key: "domain",
-        title: "Domain",
-        dataIndex: "domain",
-      },
-      {
-        key: "code",
-        title: "Code",
-        dataIndex: "code",
-      },
-    ];
+    static async getInitialProps({req}) {
+        let cityList = await cityListApi();
+        let houseList = await filterHouseApi({cityId: 1});
+        return {
+            cityList,
+            houseList,
+        };
+    }
 
-    return (
-      <>
-        <Meta {...this.meta}></Meta>
-        <Header></Header>
+    render() {
+        let {cityList, houseList} = this.props;
+        let {list} = houseList;
+        let dataSource = cityList.map((v, index) => ({
+            key: index,
+            title: v.name,
+            domain: v.domain,
+            code: v.code,
+        }));
 
-        <div className={styles.name}>Welcome to js!</div>
-        <Table
-          dataSource={dataSource}
-          columns={columns}
-          title={() => "城市列表"}
-        ></Table>
-        <ul>
-          {list.map((v) => (
-            <li className={styles.houseItem} key={v.id}>
-              <Link href={`/room/${v.id}.html`}>
-                <a>
-                  <HouseMainList {...v} />
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </>
-    );
-  }
+        const columns = [
+            {
+                key: "title",
+                title: "Title",
+                dataIndex: "title",
+            },
+            {
+                key: "domain",
+                title: "Domain",
+                dataIndex: "domain",
+            },
+            {
+                key: "code",
+                title: "Code",
+                dataIndex: "code",
+            },
+        ];
+
+        return (
+            <>
+                <Meta {...this.meta}></Meta>
+                <Header></Header>
+
+                <div className={styles.name}>Welcome to js!</div>
+                <Table
+                    dataSource={dataSource}
+                    columns={columns}
+                    title={() => "城市列表"}
+                ></Table>
+                <ul>
+                    {list.map((v) => (
+                        <li className={styles.houseItem} key={v.id}>
+                            <Link href={`/room/${v.id}.html`}>
+                                <a>
+                                    <HouseMainList {...v} />
+                                </a>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </>
+        );
+    }
 }
